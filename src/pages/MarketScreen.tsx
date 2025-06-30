34664b8b36926ad1263ae47d75deac8d997d2d96
Dixith-dev <tabrobotics@gmail.com>
2025-07-01 01:45:44 +0530

    marketbase + bg

--- src/pages/MarketScreen.tsx
+++ src/pages/MarketScreen.tsx
@@ -1,9 +1,69 @@
 import React from "react";
+import type { JSX } from "react";
+import { BottomNav3 } from "./InventoryScreenSections/BottomNav3";
 
-export const MarketScreen = () => {
+// Import background image
+import italyBgImg from "@/static/img/italybg.jpg";
+
+export const MarketScreen = (): JSX.Element => {
   return (
-    <div>
-      <h1>MarketScreen</h1>
+    <div className="min-h-screen w-full bg-black overflow-hidden">
+      {/* Mobile Layout - Exact Figma Dimensions (393x852) - up to 767px */}
+      <div className="relative w-full h-screen overflow-hidden md:hidden">
+        {/* Background Image */}
+        <div 
+          className="absolute"
+          style={{
+            width: '486px',
+            height: '851px',
+            left: '-43px',
+            filter: 'blur(2px)',
+            backgroundImage: `url(${italyBgImg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+        />
+
+        {/* MARKET Title */}
+        <div className="absolute w-[137px] h-[34px] left-[128px] top-[59px] z-20">
+          <h1 className="text-white text-[32px] font-normal leading-[34px] text-center tracking-[0.02em]" style={{ fontFamily: 'Bangers' }}>
+            MARKET
+          </h1>
+        </div>
+
+        {/* Bottom Navigation */}
+        <div className="absolute bottom-0 left-0 right-0 z-30">
+          <BottomNav3 />
+        </div>
+      </div>
+
+      {/* Desktop Layout - 768px and above */}
+      <div className="hidden md:block h-screen relative">
+        <img
+          className="absolute inset-0 w-full h-full object-cover opacity-70"
+          alt="Background"
+          src={italyBgImg}
+        />
+
+        {/* Desktop Content - Centered Layout */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center z-10 pb-20">
+          {/* Title */}
+          <div className="text-center mb-7">
+            <h1 className="text-white text-5xl font-black tracking-wider" style={{ fontFamily: 'Bangers' }}>
+              MARKET
+            </h1>
+          </div>
+        </div>
+
+        {/* Desktop Bottom Navigation */}
+        <div className="fixed bottom-0 left-0 right-0 z-30">
+          <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-30">
+            <div className="scale-60 sm:scale-70 md:scale-80 lg:scale-90 xl:scale-100 origin-bottom">
+              <BottomNav3 />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
